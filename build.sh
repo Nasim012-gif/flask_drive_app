4c39ce95ae52aa109ea9cec72f8ff518e9790289
Nasim012-gif <nasimh486@gmail.com>
2025-12-18 12:36:51 -0800

    Robust build script with explicit pip upgrade

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
-pip install -r requirements.txt
+# Upgrade pip and install dependencies
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 # Create necessary directories locally
 mkdir -p static/qr_codes
